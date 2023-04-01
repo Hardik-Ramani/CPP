5da53d50eed6ee3638b39dd2e6a123adfd5d09a2
Hardik-Ramani <hardikramani666@gmail.com>
2023-04-01 10:27:04 +0530

    hardik"

---
+++ cpp/Untitled2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+sum(int a[],int n)
+    char sum=0,i;
+    for(i=0;i<n;i++)
+    {
+    	sum+=a[i];
+	}
+	return sum;
+	
+main()
+{
+	char a[n];
+	printf("enter the size of array :");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("enter the numbers :");
+		scanf("%d",&a[i]);
+	
+	}
+	printf("the sum of all ")
+}
